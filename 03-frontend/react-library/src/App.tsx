a7f16b1ea2983416adbfeba4021138307a0c196f
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-15 11:05:53 +0200

    added navbar & top books component --v1.00

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+import Navbar from './layouts/NavbarAndFooter/Navbar';
+import ExploreTopBooks from './layouts/HomePage/ExploreTopBooks';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Successfully deployed React landing page!</h1>
-    </div>
+    <Fragment>
+      <Navbar />
+      <ExploreTopBooks />
+    </Fragment>
   );
 }
 
---
+++ 03-frontend/react-library/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ExploreTopBooks(){
+
+   return(
+      <div className="p-5 mb-4 bg-dark header">
+         <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+            <div>
+               <h1 className="display-5 fw-bold">Find your next favourite book!</h1>
+               <p className="col-md-8 fs-4">Where would you like to go next?</p>
+               <a type="button" className="btn main-color btn-lg text-white" href="#">Explore our most rated books!</a>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default ExploreTopBooks;
