911953ff6bd0f9de96af4959fe19ab4c7b528eee
Milos Damjanovic <milos.10001@yahoo.com>
2023-05-16 11:08:03 +0200

    Added ReturnBook component: To show Book carousel Split Mobile and Desktop components --v1.00

--- 03-frontend/react-library/src/layouts/HomePage/CarouselDesktopView.tsx
+++ 03-frontend/react-library/src/layouts/HomePage/CarouselDesktopView.tsx
@@ -1,46 +1,30 @@
+import ReturnBook from "./ReturnBook";
 
 function CarouselDesktopView() {
 
    return(
       <>
-         {/* Desktop */}
+         {/* Desktop reactive view */}
          <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>MDM</p>
-                  <a className="btn main-color text-white" href="#">
-                    Reserve!
-                  </a>
-                </div>
-              </div>
+               <ReturnBook />
+               <ReturnBook />
+               <ReturnBook />
             </div>
           </div>
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>MDM</p>
-                  <a className="btn main-color text-white" href="#">
-                    Reserve!
-                  </a>
-                </div>
-              </div>
+               <ReturnBook />
+               <ReturnBook />
+               <ReturnBook />
+            </div>
+          </div>
+          <div className="carousel-item">
+            <div className="row d-flex justify-content-center align-items-center">
+               <ReturnBook />
+               <ReturnBook />
+               <ReturnBook />
             </div>
           </div>
         </div>
